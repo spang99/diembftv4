82475d77a423df13956f187053ebc9a1268429aa
Sudipto Bal <bitcse.sudipto@gmail.com>
2021-10-11 02:28:18 +0000

    Change pending_block_tree implementation, add condition for checking a block before committing in BlockTree Class and add documentation to the Ledger module

--- BlockTree.py
+++ BlockTree.py
@@ -11,17 +11,27 @@ class Block:
 		printf('Created new Block class')
 
 class PendingBlockTree:
-        def __init__():
-                self.root = None
+        def __init__(b):
+                self.root = b.qc.vote_info.parent_id
                 self.blocks = {} #maps a block_id to a block
-                printf('Created new PendingBlockTree class object')
-        def add(b):
-                printf('Adding new block to PendingBlockTree class')
                 self.blocks[b.id] = b
+                printf('Created new PendingBlockTree class object')
+
+class PendingBlockTreeQueue:
+        def __init__():
+                self.tree = deque()
+                print("PendingBlockTreeQueue object created")
+        def add(b):
+                print('In function PendingBlockTreeQueue.add(); Adding new block to PendingBlockTreeQueue class')
+                t = PendingBlockTree(b)
+                self.tree.append(t)
         def prune(root):
-                printf('Pruning PendingBlockTree class')
-                self.root = root
-                self.blocks.clear()
+                print('In function PendingBlockTreeQueue.prune()')
+                if self.tree[0].root is root:
+                        printf('Pruning Block %d PendingBlockTreeQueue class', root)
+                        tree.popleft()
+                else:
+                        print("Could not prune: INCONSISTENT STATE")
 
 class LedgerCommitInfo:
 	def __init__(self, commit_state_id, vote_info_hash):
@@ -83,24 +93,27 @@ class VoteMsg:
 class BlockTree:
         high_commit_qc = None
         high_qc = None
-        pending_block_tree = PendingBlockTree() # Can be declared a dictionary also
+        pending_block_tree = PendingBlockTreeQueue()
         pending_votes = {} # Declaring as dictionary for quick search
         vote_idx = None
         f = None
+
 	def process_qc(qc):
 		print('In function: process_qc')
-		if qc.ledger_commit_info.commit_state_id == None:
+                if (qc.ledger_commit_info.commit_state_id is not None) and (qc.vote_info.parent_id not in Ledger.commits):
+                        # included additional check to prevent committing an already committed block
 			Ledger.commit(qc.vote_info.parent_id)
-			BlockTree.pending_block_tree.prune(qc.vote_info.parent_id) #replace prune
-			if qc.vote_info.round > BlockTree.high_commit_qc.vote_info.round :
-				BlockTree.high_commit_qc = qc
+			self.pending_block_tree.prune(qc.vote_info.parent_id) #replace prune
+			if qc.vote_info.round > self.high_commit_qc.vote_info.round :
+				self.high_commit_qc = qc
 		if qc.vote_info.round > BlockTree.high_qc.vote_info.round :
-				BlockTree.high_qc = qc
-	
+		        self.high_qc = qc
+
+        #sequence of the following function has been changed as we are not maintaining any speculate tree in Ledger
 	def execute_and_insert(b):
 		print('In function: execute_and_insert')
+                BlockTree.pending_block_tree.add(t)
 		Ledger.speculate(b.qc.block_id, b.id, b.payload)
-		BlockTree.pending_block_tree.add(b) #Change function to match list type later
 
         def verify_signature(v):
             verify_key = Safety.get_verify_key(v.sender)
@@ -115,15 +128,18 @@ class BlockTree:
 		print('In function: process_vote')
 		process_qc(v.high_commit_qc)
 		if verify_signature(v) is True: #implement code for signature verification
-			BlockTree.pending_votes[vote_idx].append([v.sender, v.signature])
+			self.pending_votes[vote_idx].append([v.sender, v.signature])
 			if len(BlockTree.pending_votes[vote_idx]) == 2*f + 1:
-				qc = QC(v.vote_info, v.ledger_commit_info, BlockTree.pending_votes[vote_idx], Safety.validator_id)
+				qc = QC(v.vote_info, v.ledger_commit_info, BlockTree.pending_votes[vote_idx],\
+                                        Safety.validator_id)
 				print('Created quorum certificate')
 				return qc
 		return None
 
 	def generate_block(txns, current_round):
 		print('In function: generate_block')
-		b = Block(Safety.validator_id, current_round, txns, high_commit_qc, nacl.hash.sha256([Safety.validator_id, current_round, high_commit_qc.vote_info_id, high_commit_qc.signatures]))
+		b = Block(Safety.validator_id, current_round, txns, high_commit_qc, nacl.hash.sha256\
+                        ([Safety.validator_id, current_round, high_commit_qc.vote_info_id, \
+                        high_commit_qc.signatures]))
 		#qc replaced by high_commit_qc
 		return b
