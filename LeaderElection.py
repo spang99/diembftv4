3fb282e804546ad2a8c27fbf61590d994e063836
Stella Pang <49531891+spang99@users.noreply.github.com>
2021-10-12 15:07:26 -0400

    changes of LeaderElection

--- LeaderElection.py
+++ LeaderElection.py
@@ -1,17 +1,18 @@
 from Ledger import *
 from PaceMaker import *
 
-validators = None
-window_size = None
-exclude_size = None
+validators = Validator.validators
+window_size = f
+exclude_size = f + 1
 reputation_leaders = None
 
 
-def __init__(self, validators, window_size, exclude_size, reputation_leaders):
-    self.validators = validators
-    self.window_size = window_size
-    self.exclude_size = exclude_size
-    self.reputation_leaders = reputation_leaders
+# def set_fields(vals, w_size, e_size, r_leaders):
+#     global validators, window_size, exclude_size, reputation_leaders
+#     validators = vals
+#     window_size = w_size
+#     exclude_size = e_size
+#     reputation_leaders = r_leaders
 
 
 def elect_reputation_leader(qc):
@@ -21,12 +22,11 @@ def elect_reputation_leader(qc):
     last_authors = {}
     current_qc = qc
     i = 0
-    while (i < window_size) or len(last_authors) < exclude_size:
-        current_block = Ledger.committed_block(current_qc.vote_infor.parent_id)
+    while i < window_size or len(last_authors) < exclude_size:
+        current_block = Ledger.committed_block(current_qc.vote_info.parent_id)
         block_author = current_block.author
-        if i < self.window_size:
-            # signers is a set of ids?
-            # set of validator ids
+        if i < window_size:
+            # signers is a set of validator ids
             active_validators.add(current_qc.signatures.signers())
         if len(last_authors) < exclude_size:
             # see safety validator id
@@ -34,17 +34,18 @@ def elect_reputation_leader(qc):
         current_qc = current_block.qc
         # backslash means complement set, make sure both are sets
         active_validators = active_validators - last_authors
-        i = i + 1
+        i += 1
     return active_validators.pick_one(seed=qc.vote_info.round)
 
 
 def update_leaders(qc):
     print("In function: LeaderElection.update_leaders")
+    global reputation_leaders
     extended_route = qc.vote_info.parent_round
     qc_round = qc.vote_info.round
     current_round = PaceMaker.current_round
     if (extended_route + 1 == qc_round) and (qc_round + 1 == current_round):
-        self.reputation_leaders[current_round + 1] = elect_reputation_leader(qc)
+        reputation_leaders[current_round + 1] = elect_reputation_leader(qc)
 
 
 def get_leader(round):
