6f6d24c42875fb4e226b1fc5239951223a622247
Sudipto Bal <bitcse.sudipto@gmail.com>
2021-12-02 19:48:18 -0500

    Sync up TestRun, TestExecutor and NetworkPlayground

--- src/TestRun.py
+++ src/TestRun.py
@@ -1,10 +1,15 @@
-
-
 import TestGenerator
 import TestExecutor
 
 def main():
+    nodes = ['A', 'B', 'C', 'D']
+    target_nodes = ['X']
+
     TestGenerator.initialize(['A', 'B', 'C', 'D'], ['X'])
     TestGenerator.createAllPartitions()
     TestGenerator.select_test_configurations([2, 3], 10, True, 10, True, 7)
+
+    te = new(TestExecutor.TestExecutor, num=1)
+    setup(te, nodes, target_nodes)
+    start(te)
     return
